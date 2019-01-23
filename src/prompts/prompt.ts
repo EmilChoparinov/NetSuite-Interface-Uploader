50b3a6a58c44c96d1cb57c076dea3490ee1ed78a
Choparinov <E068127@mcgladrey.rsm.net>
2019-01-23 09:49:47 -0800

    added role section

--- src/prompts/prompt.ts
+++ src/prompts/prompt.ts
@@ -1,23 +1,49 @@
 export abstract class Prompt {
 
+    text: string;
+    name: string;
     success = true;
-    answer: string;
 
-    abstract async continue(answer: string): Promise<boolean>;
-    abstract prompt(): Thenable<string>;
+    abstract async continue(answer: string, currentAggregate: objectAggregate): Promise<boolean>;
+    abstract prompt(currentAggregate: objectAggregate): Thenable<string>;
     abstract async showPrompt(): Promise<boolean>;
 
-    async runPrompt() {
-        if (await this.showPrompt()) {
-            this.answer = '';
+    public async runPrompt(currentAggregate: objectAggregate) {
+        this.success = await this.showPrompt();
+
+        if (this.success) {
+            this.text = '';
+
             do {
-                this.answer = await this.prompt();
-                if (this.answer === undefined) {
+                this.text = await this.prompt(currentAggregate);
+                if (this.text === undefined) {
                     this.success = false;
                     break;
                 }
-            } while (await this.continue(this.answer));
+
+            } while (await this.continue(this.text, currentAggregate));
         }
+
         return this.success;
+    }
+}
+
+export class PromptSeries {
+
+    series: Prompt[] = [];
+    public next(prompt: Prompt): PromptSeries {
+        this.series.push(prompt);
+        return this;
+    }
+
+    public async run(): Promise<objectAggregate> {
+        const objectAggregate: objectAggregate = {};
+
+        for (let currentPrompt of this.series) {
+            const isPromptSuccessful = await currentPrompt.runPrompt(objectAggregate);
+            if (!isPromptSuccessful) { break; }
+        }
+
+        return objectAggregate;
     }
 }
